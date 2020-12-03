9205fda956e6acd4ba7cb578f71f16e8c82a62ef
eunwoosong <songe00348@gmail.com>
2020-12-03 09:16:17 +0900

    Update Sound Manager, Text Component

---
+++ Cor_Engine/Cor_Engine/DirectXSoundManager.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <Windows.h>
+#include <mmsystem.h>
+#include <dsound.h>
+#include <map>
+
+class DirectXSoundManager
+{
+public:
+	bool Initialize(HWND hWnd);
+	void Shutdown();
+
+	bool InitializeDirectSound(HWND hWnd);
+	void ShutdownDirectSound();
+
+	bool LoadWaveFile(const char* path, const char* soundName);
+	void ShutdownWaveFile(const char* soundName);
+
+	bool PlayWaveFile(const char* soundName);
+
+private:
+	struct WaveHeaderType {
+		char chunkId[4];
+		unsigned long chunkSize;
+		char format[4];
+		char subChunkId[4];
+		unsigned long subChunkSize;
+		unsigned short audioFormat;
+		unsigned short numChannels;
+		unsigned long sampleRate;
+		unsigned long bytePerSecond;
+		unsigned short blockAlign;
+		unsigned short bitsPerSample;
+		char dataChunkId[4];
+		unsigned long dataSize;
+	};
+	IDirectSound8* directSound = nullptr;
+	IDirectSoundBuffer* primaryBuffer = nullptr;
+	std::map<const char*, IDirectSoundBuffer*> soundMap;
+};
